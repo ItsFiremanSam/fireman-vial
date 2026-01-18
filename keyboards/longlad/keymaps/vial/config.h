a384c9b53a8175615263d0b74ee31e2f3a183ced
Sam <47176495+ItsFiremanSam@users.noreply.github.com>
2026-01-18 09:12:24 +0100

    Add Long Lad keyboard

---
+++ keyboards/longlad/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x74, 0x65, 0x8E, 0x70, 0x52, 0x8E, 0x44, 0x75}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
